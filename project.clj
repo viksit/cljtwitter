0e3745838f8d93262282ed8f121fabf5980e2f5d
Viksit Gaur <viksit@rescomp-08-125572.stanford.edu>
2010-04-15 23:32:12 -0700

    First version

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject test1 "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
+                 [org.danlarkin/clojure-json "1.1-SNAPSHOT"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+  :main test1)
---
+++ src/test1.clj
@@ -0,0 +1,64 @@
+; http://p.hagelb.org/http-client-send-body
+; http://p.hagelb.org/couch.clj.html
+
+(ns test1
+   (:gen-class)
+   (:use [clojure.contrib.sql :only (with-connection with-query-results)] )
+   (:use [clojure.contrib.http.agent :only [http-agent status string]])
+   (:use [clojure.contrib.duck-streams :only (slurp*)])
+   ;(:use [clojure.http.client :only [url-encode]])
+   (:use [clojure.contrib.str-utils :only [str-join]])
+   (:use [org.danlarkin.json :only [encode-to-str decode-from-str]])
+
+   (:import (java.sql DriverManager)
+	    (java.net URL URLEncoder)
+	    (java.io StringReader InputStream)))
+
+; Variable declarations
+; Database specific
+(def +db-path+  "/Users/viksit/work/clojure/test1/test1.db")
+(def +db-specs+ {:classname  "org.sqlite.JDBC",
+                 :subprotocol   "sqlite",
+                 :subname       +db-path+})
+(def +transactions-query+ "select * from my_table")
+
+; URL
+(defn url-encode
+  "Wrapper around java.net.URLEncoder returning a (UTF-8) URL encoded
+representation of text."
+  [text]
+  (URLEncoder/encode text "UTF-8"))
+
+
+(def +url-to-fetch+ "http://search.twitter.com/search.json?q=%s")
+
+(defn construct-url [qy]
+  "Construct a url given a query"
+     (format +url-to-fetch+ qy))
+
+(defn- fetch-url [url]
+  "Fetch a URL through an HTTP agent"
+	 (http-agent url))
+	 
+(defn- collect-response [& agnts]
+  "Fetch the response from http agents"
+  (apply await agnts)
+  (for [a agnts]
+    (if (= (status a) 200)
+      (string a)
+      (status a))))
+
+(defn- twitter-search [query]
+  "Search twitter for a given query"
+  (let [agnts (fetch-url (construct-url query))]
+    (collect-response agnts)))
+
+
+(defn -main [& args]
+  "This is a sample comment"
+  (prn "Test twitter program with Sqlite")
+  (prn (twitter-search "#chirp")))
+  ;(with-connection +db-specs+
+  ;  (with-query-results results [+transactions-query+]
+  ;    ;; results is an array of column_name -> value maps
+  ;    (doall (map #(println %) results))))
